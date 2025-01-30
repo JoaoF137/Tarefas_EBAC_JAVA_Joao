e07dda79bea84f972c38b7249e6781f058080113
JoaoF137 <joaofr.oliveirajr@gmail.com>
2025-01-30 17:59:58 -0300

    Primeiro Commit

---
+++ mod8/src/mod8/Exercicio.java
@@ -0,0 +1,17 @@
+package mod8;
+
+public class Exercicio {
+
+	public static void main(String[] args) {
+		int num1 = 10;
+		int num2 = 20;
+		int num3 = 30;
+		int num4 = 40;
+		
+		double media = (num1+num2+num3+num4)/4;
+		System.out.println("A média entre os números: "+num1+","+num2+","+num3+","+num4+" é igual a "+media);
+		
+	}
+
+	
+}
